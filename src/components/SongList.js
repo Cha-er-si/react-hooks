96a25bc6a3c72071526b9f2d52d493b01fb7236f
Cha-er-si <vpcharles31@gmail.com>
2023-06-26 11:52:57 +0800

    Initial Commit and Added Song List Component

---
+++ src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SongList = () => {
+  return (
+    <div className="sing-list">
+      <uk>
+        <li>Anti-Fragile</li>
+        <li>Pop</li>
+        <li>Go Up</li>
+      </uk>
+    </div>
+  );
+};
+
+export default SongList;
